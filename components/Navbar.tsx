74fbdf6bfc72e901905c08cd22722f1c247309a2
aste425 <alex.stewart14361@gmail.com>
2023-02-20 12:03:41 -1000

    Added some working auth.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
 
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 const Navbar = () => {
+
+    const { user, error, isLoading } = useUser();
+
     return (
         <div className='flex flex-row bg-white justify-between py-2'>
             <ul className='flex flex-row'>
@@ -11,8 +16,13 @@ const Navbar = () => {
             </ul>
 
             <div>
-                <button className='text-slate-900 mx-2'>Sign up</button>
-                <button className='p-2 mx-2 bg-sky-600 rounded-md hover:bg-sky-400'>Log in</button>
+                {!user && (<Link href='/api/auth/login' className='p-2 mx-2 bg-sky-600 rounded-md hover:bg-sky-400'>Log in</Link>)}
+                {user && (
+                    <>
+                        <Link href='/' className='px-2 py-1 mx-2 bg-green-600 rounded-md hover:bg-green-500'>New Listing</Link>
+                        <button className='mx-2 text-slate-800'>Test</button>
+                    </>
+                )} 
             </div>
         </div>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }: AppProps) {
   	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<UserProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</UserProvider>
   	)
 }
